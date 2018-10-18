e83a8edde68a910de0ccf41f61fa3b2ccc801c0d
DaceKonn <dacekonn@gmail.com>
2018-10-18 22:05:12 +0200

    Truly initial

---
+++ Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace DreamOfStars.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class DreamOfStarsEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "DreamOfStarsEditorPlugin"; }
+		}
+	}
+}
